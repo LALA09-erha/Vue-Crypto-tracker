3eb58c1523c9476c1fe9d87f4637f9d3668de597
Fikri Ainun Najib <77109333+LALA09-erha@users.noreply.github.com>
2025-11-05 00:44:52 +0700

    first commit

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['vue'],
+                    chart: ['chart.js']
+                }
+            }
+        }
+    },
+    server: {
+        port: 3000
+    }
+})
